204ad5c54b59f8f22b1115bdeacd1ca3d571140f
FENGRaymond <ec.thomasfeng@gmail.com>
2018-05-12 17:21:12 +0800

    two files

---
+++ dataset/read_cifar10.py
@@ -0,0 +1,61 @@
+import pickle
+import numpy as np
+
+PREFIX = 'cifar-10-batches-py/'
+BATCH_TRAIN = [ PREFIX+'data_batch_'+str(x) for x in range(1,6)]
+BATCH_TEST = PREFIX+'test_batch'
+
+def unpickle(file):
+    """
+    Input: pickle files of different data batches
+    Output: a dictionary that has the following key-value pairs
+            b'batch_label' - b'...batch x of X'
+            b'labels' - a list of 10000 labels
+            b'data' - 10000x3072 numpy array
+            b'filenames' - a list of 10000 names
+    """
+
+    with open(file, 'rb') as fo:
+        dict = pickle.load(fo, encoding='bytes')
+        return dict
+
+def get_all_data():
+    """
+
+    :return: X_train, Y_train, X_test, Y_test
+    in
+    """
+    Xtr = []
+    Ytr = []
+    Xte = []
+    Yte = []
+
+    for pname in BATCH_TRAIN:
+        dict = unpickle(pname)
+        X = dict[b'data']
+        Y = dict[b'labels']
+        X = X.reshape(10000,3,32,32).transpose(0,2,3,1).astype('float')
+        Xtr.append(X)
+        Ytr.append(Y)
+        print(dict[b'batch_label'].decode('ascii'))
+
+    Xtr = np.concatenate(Xtr)
+    Ytr = np.concatenate(Ytr)
+    del X, Y, dict
+    print('Xtr shape: ',Xtr.shape)
+    print('Ytr shape: ',Ytr.shape)
+
+    dict = unpickle(BATCH_TEST)
+    X = dict[b'data']
+    Y = dict[b'labels']
+    X = X.reshape(10000,3,32,32).transpose(0,2,3,1).astype('float')
+    print(dict[b'batch_label'].decode('ascii'))
+    Xte = X
+    Yte = np.array(Y)
+    print('Xte:', Xte.shape, Xte.dtype)
+    print('Yte:', Yte.shape, Yte.dtype)
+
+    return Xtr, Ytr, Xte, Yte
+
+if __name__ == '__main__':
+    X_train, Y_train, X_test, Y_test = get_all_data()
---
+++ kNN/kNN_class.py
@@ -0,0 +1,105 @@
+import numpy as np
+from scipy import stats
+
+
+class kNN_classifier(object):
+    """
+    A k Nearest Neighbour with L2 distance
+
+    """
+
+    def __init__(self):
+        pass
+
+
+    def train(self, X, Y):
+        """
+        :param X: training data
+        :param Y: training labels
+        training process memorize the data and labels, without returning anything
+        """
+
+        self.X_train = X
+        self.Y_train = Y
+
+
+    def compute_distance_2loops(self, X):
+        """
+        Compute the distance between test images in X and training images in self.X_train by using nested loops
+
+        :param X: a numpy array of (num_test, D)
+        :return dists: a numpy array of (num_test, num_train). dists[i,j] is the L2 distance between ith testing images
+        and jth training images.
+        """
+        num_test = X.shape[0]
+        num_train = self.X_train.shape[0]
+        dists = np.zeros((num_test,num_train), dtype='float')
+        for i in range(num_test):
+            for j in range(num_train):
+                dists[i,j] = np.linalg.norm((X[i,:]-self.X_train[j,:]))
+
+        return dists
+
+
+    def compute_distance_1loop(self, X):
+        """
+        Compute the distance between test images in X and training images in self.X_train by using single loop
+
+        :param X: a numpy array of (num_test, D)
+        :return dists: a numpy array of (num_test, num_train). dists[i,j] is the L2 distance between ith testing images
+        and jth training images.
+        """
+
+        num_test = X.shape[0]
+        num_train = self.X_train.shape[0]
+        dists = np.zeros((num_test, num_train), dtype = 'float')
+        for i in range(num_test):
+            dists_i = np.linalg.norm((self.X_train - X[i,:]), axis = 1)
+            dists[i,:] += dists_i
+
+        return dists
+
+
+    def compute_distance_0loop(self, X):
+        """
+        Compute the distance between test images in X and training images in self.X_train using no loops at all
+
+        :param X: a numpy array of (num_test, D)
+        :return dists: a numpy array of (num_test, num_train). dists[i,j] is the L2 distance between ith testing images
+        and jth training images.
+        """
+        num_test = X.shape[0]
+        num_train = self.X_train.shape[0]
+        dists = np.zeros((num_test, num_train), dtype = 'float')
+
+        mat_mul = np.matmul(X, self.X_train.transpose())
+
+
+        X_norm = (np.linalg.norm(X, axis = 0))**2
+        Xtr_norm = (np.linalg.norm(self.X_train.transpose(), axis = 1))**2
+
+        dists = X_norm - 2*mat_mul
+        dists += Xtr_norm
+        dists = np.sqrt(dists)
+
+        return dists
+
+
+
+    def predict_labels(self, dists, k=1):
+        """
+        Predict the classes for all test images, based on the input value k and distance matrix
+
+        :param dists: a distance matrix
+        :param k: hyperparameter for k Nearest Neighbour classifier
+        :return y: a numpy array of shape (num_test, )
+        """
+        num_test = dists.shape[0]
+        dists_index = np.argsort(dists, axis = 1)[:, -k:]
+        dists_classes = self.Y_train[np.concatenate(dists_index)].reshape(num_test, -1)
+        y, _ = stats.mode(dists_classes, axis = 1)
+
+        return np.squeeze(y)
+
+
+
