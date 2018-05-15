5f35a091a96cb14ab25397b5e71b718348ccef1c
FENGRaymond <ec.thomasfeng@gmail.com>
2018-05-15 16:40:48 +0800

    started softmax+cross-entropy

---
+++ SVM/softmax.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+
+def softmax_loss_vectorized(W, X, Y, reg):
+    """
+
+    :param W:
+    :param X:
+    :param Y:
+    :param reg:
+    :return:
+    """
+
+    loss = 0.0
+    dW = np.zeros_like(W)
+
+    return loss, dW
